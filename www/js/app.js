c938e9fbbfaf331bdaa07fed5f3ccf670ba3694f
Francisco José A. C. Souza <francisceioseph@gmail.com>
2016-06-14 11:37:52 -0300

    Adding blank files for the views

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,10 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('App', ['ionic'])
 
+.config(function($stateProvider, $urlRouterProvider){
+  
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
