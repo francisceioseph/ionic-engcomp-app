16377844488fc9bef8b6c09fa2aa990bf2454326
Francisco José A. C. Souza <francisceioseph@gmail.com>
2016-06-20 08:27:27 -0300

    configurações finais no config.xml

--- www/views/mails/mails.js
+++ www/views/mails/mails.js
@@ -1,13 +1,4 @@
 angular.module('App')
-.controller('TeacherMailsController', function($scope, TeacherEmails){
+.controller('TeacherMailsController', function($scope, $ionicPopup, TeacherEmails){
   $scope.emails = TeacherEmails.emails;
-
-  $scope.openMail = function(emailAddr){
-    var email = {
-      to: emailAddr,
-      isHTML: true
-    };
-
-    window.plugin.email.open(email, this);
-  };
 });
--- www/views/phones/phones.js
+++ www/views/phones/phones.js
@@ -1,11 +1,3 @@
 angular.module('App')
 .controller('PhonesController', function($scope){
-  $scope.openMail = function(emailAddr){
-    var email = {
-      to: emailAddr,
-      isHTML: true
-    };
-
-    window.plugin.email.open(email, this);
-  };
 });
